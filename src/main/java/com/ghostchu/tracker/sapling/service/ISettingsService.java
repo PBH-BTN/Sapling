97b4ac30d50f4dcaa7e111d2bc9ae19a292b2fad
Ghost_chu <ghostchu@111.com>
2025-02-06 21:27:42 +0800

    设置表

---
+++ src/main/java/com/ghostchu/tracker/sapling/service/ISettingsService.java
@@ -0,0 +1,17 @@
+package com.ghostchu.tracker.sapling.service;
+
+import com.ghostchu.tracker.sapling.entity.Settings;
+import com.github.yulichang.base.MPJBaseService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Ghost_chu
+ * @since 2025-02-04
+ */
+public interface ISettingsService extends MPJBaseService<Settings> {
+
+    Settings getSetting(String key);
+}
---
+++ src/main/java/com/ghostchu/tracker/sapling/service/impl/SettingsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.ghostchu.tracker.sapling.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ghostchu.tracker.sapling.entity.Settings;
+import com.ghostchu.tracker.sapling.mapper.SettingsMapper;
+import com.ghostchu.tracker.sapling.service.ISettingsService;
+import com.github.yulichang.base.MPJBaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Ghost_chu
+ * @since 2025-02-04
+ */
+@Service
+public class SettingsServiceImpl extends MPJBaseServiceImpl<SettingsMapper, Settings> implements ISettingsService {
+
+    @Override
+    public Settings getSetting(String key) {
+        return getOne(new QueryWrapper<Settings>().eq("name", key));
+    }
+
+    public String getValue(String key, String defaultValue) {
+        Settings value = getSetting(key);
+        return value == null ? defaultValue : value.getValue();
+    }
+
+    public String getValue(String key) {
+        return getValue(key, null);
+    }
+
+    public boolean setValue(String key, String value) {
+        Settings setting = getSetting(key);
+        if (setting == null) {
+            setting = new Settings();
+            setting.setName(key);
+            setting.setValue(value);
+            return save(setting);
+        } else {
+            setting.setValue(value);
+            return updateById(setting);
+        }
+    }
+
+    public boolean removeValue(String key) {
+        return remove(new QueryWrapper<Settings>().eq("name", key));
+    }
+
+
+}
