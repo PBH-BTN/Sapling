e549d77ac59e5f210f1993a0a1d88dfc57b1cbda
Ghost_chu <ghostchu@111.com>
2025-02-08 23:57:37 +0800

    客户端管理表

---
+++ src/main/java/com/ghostchu/tracker/sapling/service/impl/ClientsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ghostchu.tracker.sapling.service.impl;
+
+import com.ghostchu.tracker.sapling.entity.Clients;
+import com.ghostchu.tracker.sapling.mapper.ClientsMapper;
+import com.ghostchu.tracker.sapling.service.IClientsService;
+import com.github.yulichang.base.MPJBaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Ghost_chu
+ * @since 2025-02-04
+ */
+@Service
+public class ClientsServiceImpl extends MPJBaseServiceImpl<ClientsMapper, Clients> implements IClientsService {
+
+}
